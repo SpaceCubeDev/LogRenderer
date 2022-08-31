8f13c0452c03b22f056704da7535b0effbe80aa2
Maxi-Mega <ixam.agem@gmail.com>
2022-08-31 22:19:45 +0200

    Add to the build script the possibility not to strip the binary

--- build_compress.sh
+++ build_compress.sh
@@ -10,22 +10,38 @@ if [ $# -ge 1 ] && [ -n "$1" ] && [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
     echo "      Display this help"
     echo "  --dynamic:"
     echo "      Make the binary dynamically linked"
+    echo "  --nostrip:"
+    echo "      Specify not to strip the binary"
     exit 0
 fi
 
 STATIC=" -extldflags=-static"
 TAGS=" -tags osusergo,netgo"
 STATIC_MSG=" statically"
+STRIP="strip $BINARY_FILENAME"
 
-if [ $# -ge 1 ] && [ -n "$1" ] && [ "$1" == "--dynamic" ]; then
-    # remove static 'modifiers'
-    STATIC=""
-    TAGS=""
-    STATIC_MSG=""
-fi
+while (($#)); do
+    [[ $1 = -- ]] && {
+        shift
+        break
+    }
+    [[ $1 = -?* ]] || break
+    case $1 in
+    --dynamic)
+        # remove static 'modifiers'
+        STATIC=""
+        TAGS=""
+        STATIC_MSG=""
+        ;;
+    --nostrip)
+        STRIP=true
+        ;;
+    esac
+    shift
+done
 
 echo "Building the app (v$VERSION)$STATIC_MSG ..."
 
-(cd src && go build -ldflags="-X 'main.version=$VERSION'$STATIC"$TAGS -o $BINARY_FILENAME && strip $BINARY_FILENAME && xz $BINARY_FILENAME && mv "$BINARY_FILENAME.xz" ../$OUTPUT_DIR)
+(cd src && go build -ldflags="-X 'main.version=$VERSION'$STATIC"$TAGS -o $BINARY_FILENAME && $STRIP && xz $BINARY_FILENAME && mv "$BINARY_FILENAME.xz" ../$OUTPUT_DIR)
 
 echo "Successfully exported $BINARY_FILENAME to $OUTPUT_DIR/$BINARY_FILENAME.xz !"
